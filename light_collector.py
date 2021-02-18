2570eddb080e3a6a39ea204ce6676c514ccbbf82
jdpelletier <john.pelletier@ymail.com>
2021-02-17 20:10:49 -1000

    syntax

--- light_collector.py
+++ light_collector.py
@@ -1,16 +1,16 @@
 import time
 import datetime
 
-import Util
+import util
 
 def light_collector():
     # Read and record the data
     currentDay = datetime.date.today()
-    path = Util.FolderCreate(currentDay)
+    path = util.FolderCreate(currentDay)
     if currentDay != datetime.date.today():
         currentDay = datetime.date.today()
         path = Util.FolderCreate(currentDay)
-    l = Util.servRead()
+    l = util.servRead()
     string = f"{l}"
     Util.FileWrite(path, string)
     time.sleep(288)            # wait 5 minutes
